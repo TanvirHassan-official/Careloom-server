e4b46b2f6fbf271da6244a3e6083da8572e6a05e
TanvirHassan-official <tanvirhassan0006@gmail.com>
2026-05-20 22:57:22 +0600

    cors added

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 require("dotenv").config();
 const port = process.env.PORT || 8000;
-
+const cors = require("cors");
+app.use(cors());
 app.get('/', (req, res) => {
   res.send('Hello world!');
 });
